e24071ecaf491e748ac5fb54bc1e0bbf71c71f5c
tafita37 <taf.rand37@gmail.com>
2025-03-26 01:14:56 +0300

    add(stat_piece) : Ajout des statistiques de pièces
    Ajout de statistique de ventes de pièces/mois pour une année donnée.

--- routes/manager/statRoutes.js
+++ routes/manager/statRoutes.js
@@ -41,8 +41,60 @@ router.get("/statCA", async (req, res) => {
         { $limit: 1 }, // Prendre le fournisseur avec le plus petit total
       ]);
       bestFournisseur = bestFournisseur.length > 0 ? bestFournisseur[0] : {};
-    return res.status(200).json({ bestFournisseur });
-  } catch (error) {
+
+      const anneeCA = parseInt(req.query.anneeCA);
+      if (isNaN(anneeCA)) {
+        return res
+          .status(400)
+          .json({ message: "L'année doit être un nombre valide" });
+      }
+
+      const dateFin =
+        req.query.anneeCA && req.query.anneeCA != "null"
+          ? new Date(req.query.dateFin)
+          : null;
+
+      var statPiece = await Mouvement.aggregate([
+        {
+          $match: {
+            dateMouvement: {
+              $gte: new Date(anneeCA, 0, 1), // 1er janvier de l'année
+              $lt: new Date(anneeCA + 1, 0, 1), // 1er janvier de l'année suivante
+            },
+            isEntree: false,
+          },
+        },
+        {
+          $project: {
+            mois: { $month: "$dateMouvement" },
+            calcul: { $multiply: ["$prix", { $subtract: ["$nb", "$sortie"] }] },
+          },
+        },
+        {
+          $group: {
+            _id: "$mois",
+            total: { $sum: "$calcul" },
+          },
+        },
+        {
+          $sort: { _id: 1 }, // Trier par mois croissant
+        },
+      ]);
+
+      const allMonths = Array.from({ length: 12 }, (_, i) => ({
+        _id: i + 1,
+        total: 0,
+      }));
+      // 3. Fusion des résultats avec le template
+      statPiece = allMonths.map((month) => {
+        const existingMonth = statPiece.find((item) => item._id === month._id);
+        return existingMonth || month;
+      });
+
+      // 4. Tri par mois (normalement déjà ordonné mais au cas où)
+      // statPiece.sort((a, b) => a._id - b._id);
+      return res.status(200).json({ bestFournisseur, statPiece });
+    } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Erreur." });
   }
