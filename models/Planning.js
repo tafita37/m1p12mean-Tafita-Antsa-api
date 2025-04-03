ea9c69a561530a831129ac4656edd87bfd70ef1d
antsatianaprincia <antsatianaprincia18@gmail.com>
2025-04-03 09:10:27 +0300

    add : Gestion planning

---
+++ models/Planning.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const PlanningSchema = new mongoose.Schema({
+    appointment: {type: mongoose.Schema.Types.ObjectId, ref: "RendezVous", required: true},
+    mechanic: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    date: {type: Date, required: true}
+}) 
+
+module.exports = mongoose.model("Planning",PlanningSchema);
